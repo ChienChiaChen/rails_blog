e4fcd2aa016b0bf7a3af251dc855fd407a74cdff
chiachen <s124202468@gmail.com>
2015-07-19 05:02:49 +0800

    Add comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
 	def new
 	  @post = Post.new
 	end
-	 
+	
 	def create
 	  @post = Post.new(post_params)
 	 
@@ -15,6 +15,28 @@ class PostsController < ApplicationController
 	  else
 	    render 'new'
 	  end
+
+	def edit
+	  @post = Post.find(params[:id])
+	end
+
+	def update
+	    @post = Post.find(params[:id])
+	 
+	    if @post.update(params[:post].permit(:title, :body))
+	      redirect_to @post
+	    else
+	      render 'edit'
+	    end
+	end	
+	
+	end
+
+	def destroy
+  	@post = Post.find(params[:id])
+  	@post.destroy
+ 
+  	redirect_to posts_path
 	end
 	
 	def show
